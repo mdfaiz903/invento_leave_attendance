7e314c43213bc59b170ee4b0e450a7732e5832e4
mdfaiz903 <mdfaiz.nub@gmail.com>
2025-08-20 00:17:54 +0600

    chore: add custom button and update status based on button trigger

--- invento_leave_attendance/invento_leave_attendance/doctype/leave_request/leave_request.js
+++ invento_leave_attendance/invento_leave_attendance/doctype/leave_request/leave_request.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2025, faiz and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Leave Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Leave Request", {
+	refresh(frm) {
+        if(frm.doc.status === "Pending")
+        frm.add_custom_button(__("Approve"),function(){
+            frm.set_value("status","Approved")
+            frm.set_value("approved_by",frappe.session.user)
+            frm.save()
+        })
+	},
+});
