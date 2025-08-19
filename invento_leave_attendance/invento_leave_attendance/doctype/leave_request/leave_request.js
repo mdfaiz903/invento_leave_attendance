3f3c8e2a60af7e439a1d50ff81aeb7f910155316
mdfaiz903 <mdfaiz.nub@gmail.com>
2025-08-20 01:27:36 +0600

    chore: add leave_type validation from js and calculate total_leave days exclude Saturday(6) and Sunday(0)

--- invento_leave_attendance/invento_leave_attendance/doctype/leave_request/leave_request.js
+++ invento_leave_attendance/invento_leave_attendance/doctype/leave_request/leave_request.js
@@ -10,4 +10,39 @@ frappe.ui.form.on("Leave Request", {
             frm.save()
         })
 	},
+    leave_type:function(frm){
+        if(frm.doc.leave_type==="EL"){
+            frappe.msgprint("<b>Earned Leave requires manager approval.</b>")
+        }
+    },
+    from_date:function(frm){
+        calculate_total_days(frm)
+        
+    },
+    to_date:function(frm){
+        calculate_total_days(frm)
+
+    }
 });
+function calculate_total_days(frm){
+    const from_date = frm.doc.from_date 
+    const to_date = frm.doc.to_date
+
+    console.log("########",from_date);
+    const start = frappe.datetime.str_to_obj(from_date)
+    const end = frappe.datetime.str_to_obj(to_date)
+    const current = new Date(start.getTime())
+    let total_days = frappe.datetime.get_day_diff(to_date, from_date)+1;
+    let weekend_days = 0;
+    for (current; current <= end; current.setDate(current.getDate() + 1)) {
+        let day = current.getDay();
+        if (day === 0 || day === 6) { // 0 is Sunday, 6 is Saturday
+            weekend_days++;
+        }
+    }
+
+    let working_days = total_days - weekend_days;
+    frm.set_value('total_days', working_days);
+    
+
+}
