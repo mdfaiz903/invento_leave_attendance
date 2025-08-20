d672b1d7fcc7cb394a963a3d9577ecfc521827ee
mdfaiz903 <mdfaiz.nub@gmail.com>
2025-08-21 00:29:47 +0600

    chore: leave_summary_by employee report created

---
+++ invento_leave_attendance/invento_leave_attendance/report/leave_summary_by_employee/leave_summary_by_employee.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2025, faiz and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Leave Summary by Employee"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
